45923a457fc23a9b37a97a007c6d8156370dc683
Becca Lin <becca0330@gmail.com>
2017-04-14 14:29:28 +0800

    adjust user profile validation rule

--- src/userfiles/controllers/AdminCmsUsersController.php
+++ src/userfiles/controllers/AdminCmsUsersController.php
@@ -28,11 +28,11 @@ class AdminCmsUsersController extends \crocodicstudio\crudbooster\controllers\CB
 
 		# START FORM DO NOT REMOVE THIS LINE
 		$this->form = array(); 		
-		$this->form[] = array("label"=>"Name","name"=>"name",'required'=>true,'validation'=>'required|alpha_spaces|min:3');
+		$this->form[] = array("label"=>"Name","name"=>"name",'required'=>true,'validation'=>'required|min:1');
 		$this->form[] = array("label"=>"Email","name"=>"email",'required'=>true,'type'=>'email','validation'=>'required|email|unique:cms_users,email,'.CRUDBooster::getCurrentId());		
-		$this->form[] = array("label"=>"Photo","name"=>"photo","type"=>"upload","help"=>"Recommended resolution is 200x200px",'required'=>true,'validation'=>'required|image|max:1000');											
+		$this->form[] = array("label"=>"Photo","name"=>"photo","type"=>"upload","help"=>"建议头像图片尺寸 200x200px",'required'=>true,'validation'=>'required|image|max:1000');
 		$this->form[] = array("label"=>"Privilege","name"=>"id_cms_privileges","type"=>"select","datatable"=>"cms_privileges,name",'required'=>true);						
-		$this->form[] = array("label"=>"Password","name"=>"password","type"=>"password","help"=>"Please leave empty if not change");
+		$this->form[] = array("label"=>"Password","name"=>"password","type"=>"password","help"=>"不更改密码请留白");
 		# END FORM DO NOT REMOVE THIS LINE
 				
 	}
